65dfdfa0bcf3a963d3033e064b867989a454648c
Thoinoo <thoino@outlook.fr>
2022-09-28 22:08:52 +0200

    migration character

---
+++ migrations/Version20220928200812.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220928200812 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE `character` ADD level INT NOT NULL, ADD experience INT NOT NULL, ADD health INT NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE `character` DROP level, DROP experience, DROP health');
+    }
+}
